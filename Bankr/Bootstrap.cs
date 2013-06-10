a642e90bdfe7e433a4e0555c76b6695d5d0a6c99
Chris McGrath <admin@cpmcgrath.com>
2013-06-10 14:50:53 +1000

    Updated NuGet References

--- Bankr/Bootstrap.cs
+++ Bankr/Bootstrap.cs
@@ -9,30 +9,30 @@ namespace CMcG.Bankr
     public class Bootstrap : PhoneBootstrapper
     {
         PhoneContainer container;
- 
+
         protected override void Configure()
         {
             container = new PhoneContainer(RootFrame);
- 
+
             container.RegisterPhoneServices();
             foreach (var vm in App.GetViewModelTypes())
                 container.RegisterPerRequest(vm, null, vm);
- 
+
             AddCustomConventions();
         }
- 
+
         static void AddCustomConventions()
         {
             ViewLocator.DeterminePackUriFromType = (viewModelType, viewType) =>
             {
                 var baseNamespace  = typeof(App).Namespace;
                 var uri = viewType.FullName.Replace(baseNamespace, string.Empty).Replace(".", "/") + ".xaml";
- 
+
                 if (!typeof(App).Assembly.GetAssemblyName().Equals(viewType.Assembly.GetAssemblyName()))
                 {
                     return "/" + baseNamespace + ";component" + uri;
                 }
- 
+
                 return uri;
             };
         }
@@ -42,12 +42,11 @@ namespace CMcG.Bankr
             return container.GetInstance(service, key);
         }
 
- 
         protected override IEnumerable<object> GetAllInstances(Type service)
         {
             return container.GetAllInstances(service);
         }
- 
+
         protected override void BuildUp(object instance)
         {
             container.BuildUp(instance);
